a15b49d109010921ac70c4c98e390f0d774f1ac4
lucasalc25 <100990880+lucasalc25@users.noreply.github.com>
2023-11-17 15:36:02 -0400

    4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,14 @@
-function convertTypesToLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
+
 function convertPokemonToLi (pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${convertTypesToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-            <img src="./assets/img/pokemons/${pokemon.order}.png" alt="${pokemon.name}">
+            <img src="./assets/img/pokemons/${pokemon.number}.png" alt="${pokemon.name}">
             </div>
         </li>
     `
